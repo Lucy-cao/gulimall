e2f74f7dc376bfe2a20667a72df663e0c8559585
Lucy-cao <caolu8815@163.com>
2024-06-16 06:50:46 +0800

    修改OSS policy返回值

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -1,12 +1,13 @@
 package com.atguigu.gulimall.product;
 
+import com.alibaba.cloud.spring.boot.oss.autoconfigure.OssContextAutoConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @MapperScan("com/atguigu/gulimall/product/dao")
-@SpringBootApplication
+@SpringBootApplication(exclude = {OssContextAutoConfiguration.class})
 @EnableDiscoveryClient
 public class GulimallProductApplication {
 
