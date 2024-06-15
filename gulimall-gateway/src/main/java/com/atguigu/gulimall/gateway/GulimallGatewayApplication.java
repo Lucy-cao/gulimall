1b51502db92e6aa46df57c2d45a25b4ecadd95a2
Lucy-cao <caolu8815@163.com>
2024-06-15 17:35:15 +0800

    实现服务端签名直传方法

--- gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/GulimallGatewayApplication.java
+++ gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/GulimallGatewayApplication.java
@@ -1,12 +1,13 @@
 package com.atguigu.gulimall.gateway;
 
+import com.alibaba.cloud.spring.boot.oss.autoconfigure.OssContextAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, OssContextAutoConfiguration.class})
 public class GulimallGatewayApplication {
 
     public static void main(String[] args) {
