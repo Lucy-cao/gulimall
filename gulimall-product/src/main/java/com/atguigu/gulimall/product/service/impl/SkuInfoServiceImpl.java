f0bcf15e3fb280f3adb94e0a3acea3c1d67a6249
Lucy-cao <caolu8815@163.com>
2024-07-07 10:48:42 +0800

    商品管理列表

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/SkuInfoServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/SkuInfoServiceImpl.java
@@ -1,7 +1,20 @@
 package com.atguigu.gulimall.product.service.impl;
 
+import com.atguigu.gulimall.product.service.BrandService;
+import com.atguigu.gulimall.product.service.CategoryService;
+import com.atguigu.gulimall.product.vo.SkuInfoVo;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -15,6 +28,10 @@ import com.atguigu.gulimall.product.service.SkuInfoService;
 
 @Service("skuInfoService")
 public class SkuInfoServiceImpl extends ServiceImpl<SkuInfoDao, SkuInfoEntity> implements SkuInfoService {
+    @Autowired
+    CategoryService categoryService;
+    @Autowired
+    BrandService brandService;
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -24,6 +41,45 @@ public class SkuInfoServiceImpl extends ServiceImpl<SkuInfoDao, SkuInfoEntity> i
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public PageUtils queryPageByCondition(Map<String, Object> params) {
+        //构造查询条件
+        LambdaQueryWrapper<SkuInfoEntity> wrapper = Wrappers.lambdaQuery();
+        String key = (String) params.get("key");
+        if (!StringUtils.isEmpty(key)) {
+            wrapper.like(SkuInfoEntity::getSkuName, key);
+        }
+        String catelogId = (String) params.get("catelogId");
+        if (!StringUtils.isEmpty(catelogId) && !"0".equals(catelogId)) {
+            wrapper.like(SkuInfoEntity::getCatalogId, catelogId);
+        }
+        String brandId = (String) params.get("brandId");
+        if (!StringUtils.isEmpty(brandId) && !"0".equals(brandId)) {
+            wrapper.like(SkuInfoEntity::getBrandId, brandId);
+        }
+        String min = (String) params.get("min");
+        String max = (String) params.get("max");
+        if (!StringUtils.isEmpty(min) && (new BigDecimal(min).compareTo(new BigDecimal("0")) > 0)) {
+            wrapper.ge(SkuInfoEntity::getPrice, new BigDecimal(min));
+        }
+        if (!StringUtils.isEmpty(max) && (new BigDecimal(max).compareTo(new BigDecimal("0")) > 0)) {
+            wrapper.le(SkuInfoEntity::getPrice, new BigDecimal(max));
+        }
+
+        //执行查询语句，转换为输出的格式
+        IPage<SkuInfoEntity> page = this.page(new Query<SkuInfoEntity>().getPage(params), wrapper);
+        PageUtils pageUtils = new PageUtils(page);
+        List<SkuInfoVo> skuInfoVos = page.getRecords().stream().map(skuInfoEntity -> {
+            SkuInfoVo skuInfoVo = new SkuInfoVo();
+            BeanUtils.copyProperties(skuInfoEntity, skuInfoVo);
+            skuInfoVo.setCascaderNames(categoryService.getCascaderById(skuInfoEntity.getCatalogId()).getCascaderNames());
+            skuInfoVo.setBrandName(brandService.getById(skuInfoEntity.getBrandId()).getName());
+            return skuInfoVo;
+        }).collect(Collectors.toList());
+        pageUtils.setList(skuInfoVos);
+        return pageUtils;
     }
 
 }
---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/vo/SkuInfoVo.java
@@ -0,0 +1,63 @@
+package com.atguigu.gulimall.product.vo;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class SkuInfoVo {
+
+    /**
+     * skuId
+     */
+    private Long skuId;
+    /**
+     * spuId
+     */
+    private Long spuId;
+    /**
+     * sku名称
+     */
+    private String skuName;
+    /**
+     * sku介绍描述
+     */
+    private String skuDesc;
+    /**
+     * 所属分类id
+     */
+    private Long catalogId;
+    /**
+     * 所属分类全路径名称
+     */
+    private String cascaderNames;
+    /**
+     * 品牌id
+     */
+    private Long brandId;
+    /**
+     * 品牌名称
+     */
+    private String brandName;
+    /**
+     * 默认图片
+     */
+    private String skuDefaultImg;
+    /**
+     * 标题
+     */
+    private String skuTitle;
+    /**
+     * 副标题
+     */
+    private String skuSubtitle;
+    /**
+     * 价格
+     */
+    private BigDecimal price;
+    /**
+     * 销量
+     */
+    private Long saleCount;
+}
