5281564c105b5d0f5016deaa5201710b9b58bdb0
Lucy-cao <caolu8815@163.com>
2024-11-07 05:46:20 +0800

    新增认证服务模块，实现登录和注册页面访问

---
+++ gulimall-auth-server/src/main/java/com/atguigu/gulimall/authserver/GulimallAuthServerApplication.java
@@ -0,0 +1,18 @@
+package com.atguigu.gulimall.authserver;
+
+import com.alibaba.cloud.spring.boot.oss.autoconfigure.OssAutoConfiguration;
+import com.alibaba.cloud.spring.boot.oss.autoconfigure.OssContextAutoConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {OssContextAutoConfiguration.class, DataSourceAutoConfiguration.class})
+public class GulimallAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GulimallAuthServerApplication.class, args);
+	}
+
+}
