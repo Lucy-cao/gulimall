4225878b4f739b0b0814f3b0c696d61ca49af085
Lucy-cao <caolu8815@163.com>
2024-11-08 06:31:30 +0800

    测试接入短信验证码功能

---
+++ gulimall-third-party/src/main/java/com/atguigu/gulimall/thirdparty/component/SmsComponent.java
@@ -0,0 +1,38 @@
+package com.atguigu.gulimall.thirdparty.component;
+
+import com.atguigu.gulimall.thirdparty.utils.HttpUtils;
+import lombok.Data;
+import org.apache.http.HttpResponse;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+@Component
+@ConfigurationProperties("spring.sms")
+public class SmsComponent {
+	private String host;
+	private String path;
+	private String appcode;
+
+	public void sendSmsCode(String phone, String code) {
+		String method = "GET";
+		Map<String, String> headers = new HashMap<String, String>();
+		//最后在header中的格式(中间是英文空格)为Authorization:APPCODE 83359fd73fe94948385f570e3c139105
+		headers.put("Authorization", "APPCODE " + appcode);
+		Map<String, String> querys = new HashMap<String, String>();
+		querys.put("mobile", phone);
+		querys.put("content", "【智能云】您的验证码是" + code + "。如非本人操作，请忽略本短信");
+
+		try {
+			HttpResponse response = HttpUtils.doGet(host, path, method, headers, querys);
+			System.out.println(response.toString());
+			//获取response的body
+			//System.out.println(EntityUtils.toString(response.getEntity()));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
--- gulimall-third-party/src/test/java/com/atguigu/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
+++ gulimall-third-party/src/test/java/com/atguigu/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
@@ -2,6 +2,7 @@ package com.atguigu.gulimall.thirdparty;
 
 import com.aliyun.oss.OSSClient;
 import com.aliyun.oss.OSSException;
+import com.atguigu.gulimall.thirdparty.component.SmsComponent;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,38 +12,41 @@ import java.io.InputStream;
 
 @SpringBootTest
 class GulimallThirdPartyApplicationTests {
-    @Autowired(required = false)
-    private OSSClient ossClient;
+	@Autowired(required = false)
+	private OSSClient ossClient;
+	@Autowired
+	private SmsComponent smsComponent;
 
-    @Test
-    void testUploadOss() {
-        // 填写Bucket名称，例如examplebucket。
-        String bucketName = "lucy-typora";
-        // 填写Object完整路径，例如exampledir/exampleobject.txt。Object完整路径中不能包含Bucket名称。
-        String objectName = "img/笔记本选择参考5.jpg";
+	@Test
+	void testUploadOss() {
+		// 填写Bucket名称，例如examplebucket。
+		String bucketName = "lucy-typora";
+		// 填写Object完整路径，例如exampledir/exampleobject.txt。Object完整路径中不能包含Bucket名称。
+		String objectName = "img/笔记本选择参考5.jpg";
 
-        try {
-            //上传文件流
-            InputStream inputStream = new FileInputStream("/Users/caolu/Desktop/笔记本选择参考.jpg");
-            ossClient.putObject(bucketName, objectName, inputStream);
-        } catch (OSSException oe) {
-            System.out.println("Caught an OSSException, which means your request made it to OSS, "
-                    + "but was rejected with an error response for some reason.");
-            System.out.println("Error Message:" + oe.getErrorMessage());
-            System.out.println("Error Code:" + oe.getErrorCode());
-            System.out.println("Request ID:" + oe.getRequestId());
-            System.out.println("Host ID:" + oe.getHostId());
-        } catch (Exception e) {
-            System.out.println("Error Message:" + e.getMessage());
-        } finally {
-            if (ossClient != null) {
-                ossClient.shutdown();
-            }
-        }
-    }
+		try {
+			//上传文件流
+			InputStream inputStream = new FileInputStream("/Users/caolu/Desktop/笔记本选择参考.jpg");
+			ossClient.putObject(bucketName, objectName, inputStream);
+		} catch (OSSException oe) {
+			System.out.println("Caught an OSSException, which means your request made it to OSS, "
+					+ "but was rejected with an error response for some reason.");
+			System.out.println("Error Message:" + oe.getErrorMessage());
+			System.out.println("Error Code:" + oe.getErrorCode());
+			System.out.println("Request ID:" + oe.getRequestId());
+			System.out.println("Host ID:" + oe.getHostId());
+		} catch (Exception e) {
+			System.out.println("Error Message:" + e.getMessage());
+		} finally {
+			if (ossClient != null) {
+				ossClient.shutdown();
+			}
+		}
+	}
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void testSms() {
+		smsComponent.sendSmsCode("15068168815", "876342");
+	}
 
 }
