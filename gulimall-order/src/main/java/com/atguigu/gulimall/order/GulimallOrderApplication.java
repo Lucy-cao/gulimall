c39effe87508dc6f68efa99156b56dc07c981e1e
Lucy-cao <caolu8815@163.com>
2024-12-27 06:31:28 +0800

    集成消息队列RabbitMQ

--- gulimall-order/src/main/java/com/atguigu/gulimall/order/GulimallOrderApplication.java
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/GulimallOrderApplication.java
@@ -1,13 +1,26 @@
 package com.atguigu.gulimall.order;
 
+import com.alibaba.cloud.spring.boot.oss.autoconfigure.OssContextAutoConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * RabbitMQ引入
+ * 1、引入pom依赖spring-boot-starter-amqp
+ * 2、给容器中注入组件
+ *      AmqpAdmin、RabbitTemplate、RabbitMessagingTemplate、RabbitConnectionFactoryBeanConfigurer
+ *      AmqpAdmin：新建交换器、队列、绑定关系
+ *      RabbitTemplate：发送消息
+ * 3、添加配置参数：配置参数都在RabbitProperties中，在application.yml中以spring.rabbitmq开头
+ * 4、启用RabbitMQ服务 @EnableRabbit
+ */
 @MapperScan("com/atguigu/gulimall/order/dao")
-@SpringBootApplication
+@SpringBootApplication(exclude = {OssContextAutoConfiguration.class})
 @EnableDiscoveryClient
+@EnableRabbit
 public class GulimallOrderApplication {
 
     public static void main(String[] args) {
--- gulimall-order/src/test/java/com/atguigu/gulimall/order/GulimallOrderApplicationTests.java
+++ gulimall-order/src/test/java/com/atguigu/gulimall/order/GulimallOrderApplicationTests.java
@@ -1,20 +1,88 @@
 package com.atguigu.gulimall.order;
 
 import com.atguigu.gulimall.order.entity.OrderEntity;
+import com.atguigu.gulimall.order.entity.OrderReturnReasonEntity;
 import com.atguigu.gulimall.order.service.OrderService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Date;
+
+@Slf4j
 @SpringBootTest
 class GulimallOrderApplicationTests {
-    @Autowired
-    OrderService orderService;
-    @Test
-    void contextLoads() {
-        OrderEntity order=new OrderEntity();
-        order.setMemberId(23L);
-        orderService.save(order);
-    }
+	@Autowired
+	OrderService orderService;
+	@Autowired
+	AmqpAdmin amqpAdmin;
+	@Autowired
+	RabbitTemplate rabbitTemplate;
 
+	@Test
+	void contextLoads() {
+		OrderEntity order = new OrderEntity();
+		order.setMemberId(23L);
+		orderService.save(order);
+	}
+
+	/**
+	 * 测试RabbitMQ
+	 */
+	@Test
+	public void createExchange() {
+		//创建交换器
+		//DirectExchange(String name, boolean durable, boolean autoDelete, Map<String, Object> arguments)
+		DirectExchange directExchange = new DirectExchange("hello-java-exchange", true, false);
+		amqpAdmin.declareExchange(directExchange);
+		System.out.println("directExchange = " + directExchange);
+	}
+
+	@Test
+	public void createQueue() {
+		//创建队列
+		//Queue(String name, boolean durable, boolean exclusive, boolean autoDelete,
+		//			@Nullable Map<String, Object> arguments)
+		Queue queue = new Queue("hello-java-queue", true, false, false);
+		amqpAdmin.declareQueue(queue);
+		System.out.println("queue = " + queue);
+	}
+
+	@Test
+	public void createBinding() {
+		//创建交换器和队列的绑定关系
+		//Binding(
+		// String destination, 目的地（队列名或交换器名）
+		// DestinationType destinationType,目的地类型（队列或交换器）
+		// String exchange, 交换器
+		// String routingKey, 路由键
+		//	@Nullable Map<String, Object> arguments) 其他参数
+		Binding binding = new Binding("hello-java-queue", Binding.DestinationType.QUEUE,
+				"hello-java-exchange", "hello-java-queue", null);
+		amqpAdmin.declareBinding(binding);
+		System.out.println("binding = " + binding);
+	}
+
+	@Test
+	public void sendMessageTest() {
+		//send(Message message) 直接发送消息
+		//convertAndSend(String exchange, String routingKey, Object object) 转化为字节流并发送消息
+		//1、发送字符串
+//		String msg = "hello world";
+//		rabbitTemplate.convertAndSend("hello-java-exchange", "hello-java-queue", msg);
+//		log.info("消息发送成功：{}", msg);
+
+		//2、发送实体对象
+		OrderReturnReasonEntity reasonEntity = new OrderReturnReasonEntity();
+		reasonEntity.setId(1L);
+		reasonEntity.setName("不喜欢的样式");
+		reasonEntity.setCreateTime(new Date());
+		reasonEntity.setStatus(1);
+		reasonEntity.setSort(1);
+		rabbitTemplate.convertAndSend("hello-java-exchange", "hello-java-queue", reasonEntity);
+		log.info("消息发送成功：{}", reasonEntity);
+	}
 }
